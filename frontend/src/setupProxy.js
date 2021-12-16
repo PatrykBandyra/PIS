672f6b770b4dbd548e44953384bab3427f2c2884
Bartosz Sosnowski <bartosz.sosnowski3.stud@pw.edu.pl>
2021-12-16 20:08:14 +0100

    Frontend connects to backend correctly
    PP-3

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,18 @@
+// const proxy = require('http-proxy-middleware');
+// module.exports = function(app) {
+//     app.use(proxy('/api', 
+//         { target: 'http://localhost:9000/' }
+//     ));
+// }
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api/*',
+    createProxyMiddleware({
+      target: 'http://localhost:9000',
+      secure: false,
+    })
+  );
+};
