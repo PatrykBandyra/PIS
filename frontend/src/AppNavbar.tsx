6f00eb6b62285483379cd0cd741fd45d3eb56e05
Bartosz Sosnowski <bartosz.sosnowski3.stud@pw.edu.pl>
2021-12-19 17:33:36 +0100

    PP-10 Modify AppNavbar to use react-bootstrap

--- frontend/src/AppNavbar.tsx
+++ frontend/src/AppNavbar.tsx
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Container} from 'reactstrap';
+import {Navbar, Nav} from 'react-bootstrap';
 
 type State = {
     isOpen: boolean;
 }
 
 export default class AppNavbar extends Component<{}, State> {
-    constructor(props) {
+    constructor(props: {}) {
         super(props);
-        this.state = { isOpen: false };
+        this.state = {isOpen: false};
         this.toggle = this.toggle.bind(this);
     }
 
@@ -20,10 +20,18 @@ export default class AppNavbar extends Component<{}, State> {
     }
 
     render() {
-        return <Navbar color="dark" dark expand="md">
-            <NavbarBrand tag={Link} to="/">Home</NavbarBrand>
-            <NavbarBrand tag={Link} to="/scrapeit/text">Text</NavbarBrand>
-            <NavbarBrand tag={Link} to="/scrapeit/header">Header</NavbarBrand>
-        </Navbar>;
+        return <Navbar bg="dark" expand="lg">
+            <Container>
+                <Navbar.Brand href="/">Scrape it!</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/scrapeit/text">Text</Nav.Link>
+                    <Nav.Link href="/scrapeit/header">Header</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
     }
 }
