1afc68cffe614093a3192e9daaeae0dc1ec3ddff
Eryk Wrzosek <e.wrzosek@stud.elka.pw.edu.pl>
2022-01-25 14:05:35 +0100

    Run error fix

--- backend/src/main/kotlin/com/example/pis/SecurityConfiguration.kt
+++ backend/src/main/kotlin/com/example/pis/SecurityConfiguration.kt
@@ -3,9 +3,10 @@ package com.example.pis
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
-
+@EnableWebSecurity
 @Configuration
 class SecurityConfiguration : WebSecurityConfigurerAdapter() {
     @Throws(Exception::class)
